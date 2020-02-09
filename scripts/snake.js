e71a59cab2af381dc6bca0c50fdc2fc44c566150
Marine Heckler <marine.heckler@gmail.com>
2020-02-09 18:11:54 +0100

    Snake game - draw apple.

--- scripts/snake.js
+++ scripts/snake.js
@@ -2,6 +2,10 @@ var canvas = document.querySelector("#snakeCanvas");
 var messageDisplay = document.querySelector("#snakeMsg");
 
 var ctx;
+var blockSize = 30;
+var widthInBlocks = canvas.width/blockSize;
+var heightInBlocks = canvas.height/blockSize;
+var radius = blockSize/2;
 var score;
 
 init();
@@ -10,4 +14,16 @@ function init(){
   ctx = canvas.getContext("2d");
   score = 0;
   messageDisplay.textContent = "Score : " + score;
+}
+
+function Apple(){
+  this.draw = function(){
+    var x = (Math.round(Math.random() * widthInBlocks))*blockSize + radius;
+    var y = (Math.round(Math.random() * heightInBlocks))*blockSize + radius;
+
+    ctx.fillStyle = "#51a149";
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2*Math.PI);
+    ctx.fill();
+  };
 }
